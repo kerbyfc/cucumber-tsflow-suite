cb52f7b0bbf8f78ead060433f5442a577a92a356
Alexander Lukin <alexander.lukin@acronis.com>
2016-07-21 18:05:49 +0300

    refactoring and improvement of navigation steps

--- src/steps/navigation.ts
+++ src/steps/navigation.ts
@@ -3,16 +3,26 @@
 import * as webdriver from 'selenium-webdriver';
 /* tslint:enable */
 
-import {binding, given} from 'cucumber-tsflow/dist/index';
+import {binding, given, when} from 'cucumber-tsflow/dist/index';
 import {driver} from '../support/driver';
 import Promise = webdriver.promise.Promise;
 
 @binding()
 class Navigation {
 
-    @given(/^зайти на страницу (.*)$/)
+    @given(/^открыта страница (.*)$/)
+    public async onPage(url: string): Promise<void> {
+        return await this.navigateToUrl(url);
+    }
+
+    @when(/(?:обнов|перезагруз)ить страницу/)
+    public async reloadPage(): Promise<void> {
+        return await driver.navigate().refresh();
+    }
+
+    @when(/^(?:за|пере)йти на страницу (.*)$/)
     public async navigateToPage(url: string): Promise<void> {
-        await driver.get(url);
+        return await this.navigateToUrl(url);
     }
 
     /**
@@ -21,6 +31,10 @@ class Navigation {
     @given(/^подождать (\d+) секунд.?$/)
     public awaitXSeconds(seconds: number, callback): void {
         setTimeout(callback, seconds * 1000);
+    }
+
+    protected async navigateToUrl(url: string): Promise<void> {
+        return await driver.get(url);
     }
 
 }
