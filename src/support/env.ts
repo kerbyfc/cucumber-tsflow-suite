d5d970dc09523a604369da04199f67154fcc8917
Alexander Lukin <alexander.lukin@acronis.com>
2016-07-21 13:45:39 +0300

    correct process termination

---
+++ src/support/env.ts
@@ -0,0 +1,11 @@
+import {driver} from "./driver";
+
+const configure = async function() {
+    this.setDefaultTimeout(30 * 1000);
+
+    this.registerHandler("AfterFeatures", function (event, callback) {
+        driver.quit().then(() => callback());
+    });
+};
+
+module.exports = configure;
