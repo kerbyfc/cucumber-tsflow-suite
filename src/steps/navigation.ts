3847ea4102f324f9cb31742a6a5b3c0a33e7ccc8
Alexander Lukin <alexander.lukin@acronis.com>
2016-07-21 13:45:23 +0300

    navigation steps

---
+++ src/steps/navigation.ts
@@ -0,0 +1,24 @@
+import * as webdriver from "selenium-webdriver";
+import {binding, given} from "cucumber-tsflow/dist/index";
+import {driver} from "../support/driver";
+import Promise = webdriver.promise.Promise;
+
+@binding()
+class Navigation {
+
+    @given(/^зайти на страницу "([^"]*)"$/)
+    public async navigateToPage(url: string): Promise<void> {
+        await driver.get(url);
+    }
+
+    /**
+     * TODO move to another file
+     */
+    @given(/^подождать (\d+) секунды?$/)
+    public awaitXSeconds(seconds: number, callback): void {
+        setTimeout(callback, seconds * 1000);
+    }
+
+}
+
+export = Navigation;
